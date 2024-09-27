3d252da7801d03c716935de7f5f1c1a61479125c
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2024-09-27 19:36:25 -0400

    even odd program

--- main.go
+++ main.go
@@ -34,6 +34,7 @@ func main() {
 	// Output the area
 	fmt.Printf("The area of the rectangle is: %.2f\n", area)
 
+	//  Sahil
 	var number int
 	fmt.Print("Enter a number to check if it is Odd or Even: ")
 	fmt.Scan(&number)
