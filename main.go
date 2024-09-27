e9a3903abbc6c07ba81ecbe9ab849b49ff0de16e
varis thind <vtkamboj@gmail.com>
2024-09-27 15:47:32 -0400

    prime numebr

--- main.go
+++ main.go
@@ -6,4 +6,11 @@ func main() {
 	//sukhjit
 	maxNumber := max(4, 17)
 	fmt.Println("Maximum of 42 and 17 is:", maxNumber)
+	//varis
+	num := 29
+	if isPrime(num) {
+		fmt.Printf("%d is a prime number.\n", num)
+	} else {
+		fmt.Printf("%d is not a prime number.\n", num)
+	}
 }
